417c44c9989bd8bece805628f4a8eee8eaee68e2
lhupalo <lhpzl@protonmail.com>
2021-12-11 15:01:22 -0300

    update: getting rssi from btmgmt scan

--- rssibt.sh
+++ rssibt.sh
@@ -6,25 +6,25 @@ output_path="./bt_RSSI_output.txt"
 test -f $temporary_path && rm $temporary_path
 test -f $output_path && rm $output_path
 
-addr=$(hcitool con | awk '{print $3}')
-cmd="sudo hcitool rssi"
-cmd=$cmd$addr
+cmd="sudo btmgmt find"
+cmd=$cmd
 
 echo $1' medidas a serem realizadas'
 
 n=1
 while [ $n -le $1 ]
 do
-        echo $cmd | bash >> $temporary_path
+	tmp=$(eval "$cmd")
+	echo "$tmp" >> $temporary_path
         echo 'Realizando Medida '$n
-        sleep 1s
+        sleep 3s
         ((n=$n+1))
         tput cuu1
         tput el
 done
 
 echo 'Bluetooth RSSI [dbm]' >> $output_path
-fgrep -w RSSI $temporary_path | awk '{print $NF}' >> $output_path
+fgrep -w "EDR rssi" $temporary_path | awk '{print $7}' >> $output_path
 
 rm $temporary_path
 echo "Medidas finalizadas!"
